6363e6a152f847b51768d4f2d7cc7edefbe5c365
Jamespd <Jamespd84@gmail.com>
2019-02-01 16:04:55 +0000

    gym session and member models both working and tested

--- body_burner_gym/db/seeds.rb
+++ body_burner_gym/db/seeds.rb
@@ -3,62 +3,63 @@ require_relative( "../models/member.rb" )
 require_relative( "../models/booking.rb" )
 require('pry')
 
-# GymSession.delete_all()
-Member.delete_all()
+
+GymSession.delete_all()
+# Member.delete_all()
 # Booking.delete_all()
 
 
-# gymsession1 = GymSession.new({
-#   "name" => "metafit",
-#   "type" => "cardio"
-# })
+gymsession1 = GymSession.new({
+  "name" => "metafit",
+  "type" => "cardio"
+})
+
+gymsession1.save()
+
+gymsession2 = GymSession.new({
+  "name" => "attack",
+  "type" => "combat_cardio"
+})
+
+gymsession2.save()
+
+gymsession3 = GymSession.new({
+  "name" => "deadlift",
+  "type" => "weight_training"
+})
+
+gymsession3.save()
+
+gymsession4 = GymSession.new({
+  "name" => "circuits",
+  "type" => "cardio_and_muscle_building"
+})
+
+gymsession4.save()
+
+# member1 = Member.new({'name' => 'Arnold Strong', 'age' => '25'})
+# member1.save
 #
-# gymsession1.save()
+# member2 = Member.new({'name' => 'Luis Liftmore', 'age' => '27'})
+# member2.save
 #
-# gymsession2 = GymSession.new({
-#   "name" => "attack",
-#   "type" => "combat_cardio"
-# })
+# member3 = Member.new({'name' => 'Sean McGintey', 'age' => '31'})
+# member3.save
 #
-# gymsession2.save()
+# member4 = Member.new({'name' => 'Bobby Builder', 'age' => '29'})
+# member4.save
 #
-# gymsession3 = GymSession.new({
-#   "name" => "deadlift",
-#   "type" => "weight_training"
-# })
+# member5 = Member.new({'name' => 'Sarah McFitz', 'age' => '26'})
+# member5.save
 #
-# gymsession3.save()
+# member6 = Member.new({'name' => 'Ciorstidh MacPowers', 'age' => '24'})
+# member6.save
 #
-# gymsession4 = GymSession.new({
-#   "name" => "circuits",
-#   "type" => "cardio_and_muscle_building"
-# })
+# member7 = Member.new({'name' => 'Siobhan MacStanamally', 'age' => '21'})
+# member7.save
 #
-# gymsession4.save()
-
-member1 = Member.new({'name' => 'Arnold Strong', 'age' => '25'})
-member1.save
-
-member2 = Member.new({'name' => 'Luis Liftmore', 'age' => '27'})
-member2.save
-
-member3 = Member.new({'name' => 'Sean McGintey', 'age' => '31'})
-member3.save
-
-member4 = Member.new({'name' => 'Bobby Builder', 'age' => '29'})
-member4.save
-
-member5 = Member.new({'name' => 'Sarah McFitz', 'age' => '26'})
-member5.save
-
-member6 = Member.new({'name' => 'Ciorstidh MacPowers', 'age' => '24'})
-member6.save
-
-member7 = Member.new({'name' => 'Siobhan MacStanamally', 'age' => '21'})
-member7.save
-
-member8 = Member.new({'name' => 'Mike runnerman', 'age' => '21'})
-member8.save
+# member8 = Member.new({'name' => 'Mike runnerman', 'age' => '21'})
+# member8.save
 
 # booking1 = Booking.new({'member_id' => member1.id, 'gym_session_id' => gym_session1.id})
 # booking1.save
