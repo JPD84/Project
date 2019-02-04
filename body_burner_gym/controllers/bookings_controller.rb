309cec765812ccb5a74b115f42bc93d2d4e2205e
Jamespd <Jamespd84@gmail.com>
2019-02-04 13:44:58 +0000

    Almost finished bookings

--- body_burner_gym/controllers/bookings_controller.rb
+++ body_burner_gym/controllers/bookings_controller.rb
@@ -11,6 +11,10 @@ get '/bookings' do
   erb(:"bookings/index")
 end
 
+get 'bookings/:id' do
+  erb(:"bookings/show")
+end
+
 get '/bookings/new' do
   @bookings = Booking.all
   erb(:"bookings/new")
@@ -18,5 +22,17 @@ end
 
 post '/bookings' do
   Booking.new(params).save
+  redirect to '/bookings'
+end
+
+
+post '/bookings' do
+  Booking.new(params).save
+  redirect to '/bookings'
+end
+
+post '/bookings/:id/delete' do
+  gym_session = Booking.find(params['id'])
+  gym_session.delete
   redirect to '/bookings'
 end
--- body_burner_gym/views/bookings/index.rb
+++
@@ -1,9 +0,0 @@
-<h1>All Bookings</h1>
-
-<% @bookings.each do |booking| %>
-  <p>Name: <%= gym_session.name %></p>
-  <p>Type: <%= gym_session.type %></p>
-  <p><a href="/gym_sessions/<%= gym_session.id %>">Show Gym Sessions</a></p>
-  <hr />
-
-<% end %>
